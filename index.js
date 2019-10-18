e3dfb4d879a4ef5a253afd283db540deefbdb3fa
Kevin Karma <kevinkarma55@gmail.com>
2019-10-18 17:13:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+  
+  static sanitize(word){
+    //- ' space 
+    return word.replace(/[^a-z\ '-]/gim,"");
+  }
 }
